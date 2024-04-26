cdddc448b63445a07d58f178bf2ff0af97d86058
MostlyEarlyBird <162125123+MostlyEarlyBird@users.noreply.github.com>
2024-04-26 12:37:35 +0000

    error handle

--- conf_yml.go
+++ conf_yml.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
 	"path/filepath"
 
@@ -26,21 +25,33 @@ type Yaml struct {
 	Mods map[string]Mod `yaml:"modules,flow"`
 }
 
-func (yml *Yaml) readConf(filename string) {
+func (yml *Yaml) readConf(filename string) error {
 	buf, err := os.ReadFile(filename)
 	if err != nil {
-		log.Fatal("file")
+		return err
 	}
 	err = yaml.Unmarshal(buf, &yml)
 	if err != nil {
-		log.Fatalf("in file %q: %v", filename, err)
+		return err
 	}
+	return nil
 }
 
 func main() {
 	yml := new(Yaml)
-	file, err := filepath.Glob("config.y?(a)ml")
-	yml.readConf("conf.yml")
+	file, err := filepath.Glob("config.*ml")
+	fmt.Printf("file: %v\n", file)
+	if err != nil {
+		fmt.Printf("err: %v\n", err)
+		return
+	} else if len(file) == 0 || len(file) > 1 {
+		fmt.Printf("Found: %v config files\n", len(file))
+		return
+	}
+	if err := yml.readConf(file[0]); err != nil {
+		fmt.Printf("err: %v\n", err)
+		return
+	}
 	fmt.Printf("yml: %v\n", yml)
 	for _, tag := range yml.Tags {
 		fmt.Printf("tag.Name: %v\n", tag.Name)
